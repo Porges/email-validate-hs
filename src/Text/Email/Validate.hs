84ae90f363b4426f2b7a275c50c62ac29817e4c0
George Pollard <porges@porg.es>
2014-11-19 22:15:10 +1300

    Linting and tidying up the Validate module

--- src/Text/Email/Validate.hs
+++ src/Text/Email/Validate.hs
@@ -1,26 +1,23 @@
 module Text.Email.Validate
-	(isValid
-	,validate
-	,emailAddress
-	,canonicalizeEmail 
-	,EmailAddress -- re-exported
-	,localPart
-	,domainPart
-	,toByteString) 
+	( isValid
+	, validate
+	, emailAddress
+	, canonicalizeEmail 
+	, EmailAddress -- re-exported
+	, localPart
+	, domainPart
+	, toByteString
+	)
 where
 
-import qualified Data.ByteString.Char8 as BS
 import Data.ByteString (ByteString)
 import Data.Attoparsec (parseOnly)
 
-import Text.Email.Parser
+import Text.Email.Parser (EmailAddress, toByteString, addrSpec, localPart, domainPart)
 
 -- | Smart constructor for an email address
 emailAddress :: ByteString -> Maybe EmailAddress
-emailAddress x =
-	case validate x of
-		Left _  -> Nothing
-		Right em -> Just em
+emailAddress = either (const Nothing) Just . validate
 
 -- | Checks that an email is valid and returns a version of it
 --   where comments and whitespace have been removed.
@@ -30,8 +27,7 @@ canonicalizeEmail = fmap toByteString . emailAddress
 -- | Validates whether a particular string is an email address
 --   according to RFC5322.
 isValid :: ByteString -> Bool
-isValid x = let result = validate x in
-	either (const False) (const True) result
+isValid = either (const False) (const True) . validate
 
 -- | If you want to find out *why* a particular string is not
 --   an email address, use this.
