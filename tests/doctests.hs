c56e516e1e3f799978d9e28720c8abbb5cfb708a
George Pollard <porges@porg.es>
2022-11-23 22:19:20 +0000

    Improve doctest setup

--- tests/doctests.hs
+++
@@ -1,9 +0,0 @@
-import Test.DocTest
-
-main = doctest
-    [ "-isrc"
-    , "src/Text/Email/QuasiQuotation.hs"
-    , "src/Text/Email/Validate.hs"
-    , "-XQuasiQuotes"
-    , "-XOverloadedStrings"
-    ]
